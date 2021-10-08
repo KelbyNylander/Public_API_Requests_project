7d561cc21375330db4cd8c4ffaded25733303bbe
KelbyNylander <kelbyn13@yahoo.com>
2021-10-07 20:39:24 -0400

    added functions to meets expectations

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
+let body = document.querySelector('body');
 let searchContainer = document.getElementsByClassName('search-container');
 let gallery = document.getElementById('gallery');
+let cards = document.getElementsByClassName('card');
 
 
+// ------------------------------------------
+//  FETCH FUNCTION
+// ------------------------------------------
 
-fetch('https://randomuser.me/api/?results=12')
+fetch('https://randomuser.me/api/?results=12') 
     .then(response => response.json())
-    .then(data => displayGallery(data))
-    
+    .then(data => {
+        displayGallery(data)
+        clickedModal(data.results)
+    })
+    .catch(error => (gallery.innerHTML += `<h3>There was an error fetching the data.</h3>`, error))
 
-    function displayGallery(data) {
+// ------------------------------------------
+//  DISPLAY FUNCTIONS
+// ------------------------------------------
+
+//loops though fetched data and displays gallery
+function displayGallery(data) {
     data.results.map( item => { 
         gallery.insertAdjacentHTML('beforeend', 
          `<div class="card">
@@ -23,30 +36,73 @@ fetch('https://randomuser.me/api/?results=12')
         </div>`
         )
     })
-    return data
  }
 
+//puts selected card in body of HTML 
+function displayModalWindow (profile) {
+    body.insertAdjacentHTML('beforeend', 
+        `<div class="modal-container">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src=${profile.picture.large} alt="profile picture">
+                <h3 id="name" class="modal-name cap">${profile.name.first} ${profile.name.last}</h3>
+                <p class="modal-text">${profile.email}</p>
+                <p class="modal-text cap">${profile.location.city}</p>
+                <hr>
+                <p class="modal-text">${formatPhoneNumber(profile.phone)}</p>
+                <p class="modal-text">${profile.location.street.number} ${profile.location.street.name}, ${profile.location.city}, ${profile.location.state} ${profile.location.postcode}</p>
+                <p class="modal-text">Birthday: ${formatDob(profile.dob.date)}</p>
+            </div>
+        </div>
 
-    function sayHi () {
-        console.log('hi');
-    };
+        // IMPORTANT: Below is only for exceeds tasks 
+        <div class="modal-btn-container">
+            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+        </div>
+    </div>`)
 
-let cards = document.getElementsByClassName('card');
+let modalContainer = document.querySelector('.modal-container');
+const modalCloseBtn = document.getElementById('modal-close-btn');
+modalCloseBtn.addEventListener('click', () => {
+    modalContainer.remove()
+    });
+}
 
-//let cardArray = [...cards];
+// ------------------------------------------
+//  HELPER FUNCTIONS
+// ------------------------------------------
 
-//let cardArray = Array.from(cards);
+function clickedModal (data) {
+    let cardArray = [...cards];
+        for ( let i = 0; i < data.length; i++) {
+            cardArray[i].addEventListener('click', () => {
+                let cardData = data[i]
+                displayModalWindow(cardData);
+        })
+    }
+}
 
-//let cardArray = [].slice.call(cards);
+// ------------------------------------------
+//  FORMATING FUNCTIONS
+// ------------------------------------------
 
-// let cardArray = []
-// for (i=0; i<cards.length; i++) {
-//     console.log(cards[i]);
-//     cardArray.push(cards[i]);
-// };
+function formatPhoneNumber(profileNumber) {
+  let cleaned = ('' + profileNumber).replace(/\D/g, '');
+  let match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+  if (match) {
+    return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+  };
+  return profileNumber
+  }
 
-console.log(cards);
-console.log(cardArray);
-console.log(cardArray.length);
-
-//gallery.addEventListener( 'click', sayHi);
+function formatDob(profileDOB) {
+    const chars = profileDOB.split('');
+    let dobArray = [];
+    for ( i = 0; i <= 9; i++) {
+        dobArray.push(chars[i]);
+    }
+    const dobString = dobArray.join('');
+    return dobString
+}
